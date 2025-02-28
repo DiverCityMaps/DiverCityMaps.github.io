69496c19dbe49b5a2ac469fbd475a6f6d3eb216a
Giuliano Cornacchia <37023673+GiulianoCornacchia@users.noreply.github.com>
2025-02-28 12:41:32 +0100

    Improved coloring

--- assets/js/divercity.js
+++ assets/js/divercity.js
@@ -209,9 +209,15 @@ function initializeGraphNetwork(RoadsData) {
 
 function styleRoads(feature) {
     return feature.properties.is_attractor === 1
-        ? { color: "orange", weight: 5 }
-        : { color: "grey", weight: 0.5 };
+        ? { color: "#FFE5B4", weight: 3}
+        : { color: "#D3D3D3", weight: 0.35 };
 }
+
+//function styleRoads(feature) {
+//    return feature.properties.is_attractor === 1
+//        ? { color: "#D0D0D0", weight: 1.5 }
+//        : { color: "#D3D3D3", weight: 0.35 };
+//}
 
 function filterLineString(feature) {
     return feature.geometry.type === "LineString";
